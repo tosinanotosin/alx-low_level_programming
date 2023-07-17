78d50f61f8eb3580773e3b75949626e0a9a60796
tosinanotosin <waristosin01@gmail.com>
2023-07-17 14:35:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positivie",n);
+printf("%d is positivie", n);
 }
 else if (n == 0)
 {
-printf("%d is zero",n);
+printf("%d is zero", n);
 }
 else
 {
-printf("%d is negative",n);
+printf("%d is negative", n);
 }
 return (0);
 }
