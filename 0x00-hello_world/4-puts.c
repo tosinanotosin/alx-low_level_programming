c8f5e854e87637233df4d29f4127d9ffd74cdd20
tosinanotosin <waristosin01@gmail.com>
2023-07-17 07:44:57 -0700

    6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdioo.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"progreamming is like building a multilingual puzzle");
+	return (0);
+}
